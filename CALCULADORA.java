f447840c752e04b88334bcdf13fe518ee56c292d
Larian <larian.dam@gmail.com>
2024-10-23 22:12:06 +0100

    Update CALCULADORA.java

--- CALCULADORA.java
+++ CALCULADORA.java
@@ -11,20 +11,22 @@ public class CALCULADORA {
         
         int operation = Integer.parseInt(JOptionPane.showInputDialog("(1) Sumar\n(2) Restar\n(3) Multiplicar\n(4) Dividir\nElija una opción:")); // Mostrando las operaciones disponibles
         
-        num1 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el primer número")); // Pidiendo los números
-        num2 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el segundo número"));
-        
-        if (operation == 1){ // Suma
-            JOptionPane.showMessageDialog(null, num1 + num2);
-        }
-        else if (operation == 2){ // Resta
-            JOptionPane.showMessageDialog(null, num1 - num2);            
-        }
-        else if (operation == 3){ // Multiplicación
-            JOptionPane.showMessageDialog(null, num1 * num2);            
-        }
-        else if (operation == 4){ // División
-            JOptionPane.showMessageDialog(null, num1 / num2);            
+        if (operation <=4 && operation != 0){
+            
+            num1 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el primer número")); // Pidiendo los números
+            num2 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el segundo número"));            
+            
+            if (operation == 1){ // Suma
+                JOptionPane.showMessageDialog(null, num1 + num2);
+            } else if (operation == 2){ // Resta
+                JOptionPane.showMessageDialog(null, num1 - num2);            
+            } else if (operation == 3){ // Multiplicación
+                JOptionPane.showMessageDialog(null, num1 * num2);            
+            } else if (operation == 4){ // División
+                JOptionPane.showMessageDialog(null, num1 / num2);            
+            }            
+        } else {
+            JOptionPane.showMessageDialog(null,"Opción no valida.");
         }
     }
 }
